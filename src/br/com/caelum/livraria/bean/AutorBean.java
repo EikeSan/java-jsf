c48e9f90b2d26e441ce751f6ba5df2251ce634e8
EikeSan <eikesantz@hotmail.com>
2018-06-01 11:00:40 -0300

    feat(Autor): CRUD de autor

--- src/br/com/caelum/livraria/bean/AutorBean.java
+++ src/br/com/caelum/livraria/bean/AutorBean.java
@@ -1,11 +1,16 @@
 package br.com.caelum.livraria.bean;
 
+import java.util.List;
+
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
 
+
 @ManagedBean
+@ApplicationScoped
 public class AutorBean {
 
 	private Autor autor = new Autor();
@@ -22,5 +27,30 @@ public class AutorBean {
 		this.autor = new Autor();
 		
 		return("livro?faces-redirect=true");
+	}
+	
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public void remover(Autor autor) {
+
+		new DAO<Autor>(Autor.class).remove(autor);
+		this.autor = new Autor();
+	}
+
+	public String alterar(Autor autor) {
+		this.autor = autor;
+		System.out.println(this.autor.getNome());
+		return ("carregarAutor?faces-redirect=true");
+	}
+	
+	public String update() {
+		
+		new DAO<Autor>(Autor.class).atualiza(this.autor);
+
+		this.autor= new Autor();
+		
+		return("autor?faces-redirect=true");
 	}
 }
